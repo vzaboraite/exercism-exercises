c13fffa8debab32657bdedd75b2ad5c97724984c
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-01-23 23:17:02 +0000

    Pass first 8 tests. Todo: start on `test "with apostrophes"`

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,3 +1,17 @@
-export const countWords = () => {
-  throw new Error("Remove this statement and implement this function");
+export const countWords = (string) => {
+  const words = string.trim().split(/[\s,:!&@$%^]+/i);
+  const result = {};
+  words.forEach((word) => {
+    const lowerCasedWord = word.toLowerCase();
+    if (lowerCasedWord === "") {
+      return;
+    }
+    if (result.hasOwnProperty(lowerCasedWord)) {
+      result[lowerCasedWord] += 1;
+    } else {
+      result[lowerCasedWord] = 1;
+    }
+  });
+  console.log(result, { words, string });
+  return result;
 };
