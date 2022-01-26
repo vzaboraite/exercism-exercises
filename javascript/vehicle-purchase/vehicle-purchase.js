079545923cf0780875fe01806f1b1a5c36b6ef72
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-01-26 23:01:45 +0000

    Write chooseVehicle function, pass the tests.

--- javascript/vehicle-purchase/vehicle-purchase.js
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -23,7 +23,9 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  throw new Error("Please implement the chooseVehicle function");
+  const advice = "is clearly the better choice.";
+
+  return option1 < option2 ? `${option1} ${advice}` : `${option2} ${advice}`;
 }
 
 /**
