f751a303ffbf23543625af28355050f756a8baab
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-02-01 11:00:37 +0000

    Write removePlayer(), pass the tests.

--- javascript/high-score-board/high-score-board.js
+++ javascript/high-score-board/high-score-board.js
@@ -35,7 +35,9 @@ export function addPlayer(scoreBoard, player, score) {
  * @returns {Record<string, number>} updated score board
  */
 export function removePlayer(scoreBoard, player) {
-  throw new Error("Please implement the removePlayer function");
+  delete scoreBoard[player];
+
+  return scoreBoard;
 }
 
 /**
