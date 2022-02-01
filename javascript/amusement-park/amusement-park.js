1e37ae959ab19a247407ac3add98455405e8d558
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-02-01 11:58:52 +0000

    Write createVisitor(), pass the test.

--- javascript/amusement-park/amusement-park.js
+++ javascript/amusement-park/amusement-park.js
@@ -10,7 +10,15 @@
  * @returns {Visitor} the visitor that was created
  */
 export function createVisitor(name, age, ticketId) {
-  throw new Error('Please implement the createVisitor function.');
+  let visitor = null;
+
+  visitor = {
+    name,
+    age,
+    ticketId,
+  };
+
+  return visitor;
 }
 
 /**
@@ -20,7 +28,7 @@ export function createVisitor(name, age, ticketId) {
  * @returns {Visitor} the visitor without a ticket
  */
 export function revokeTicket(visitor) {
-  throw new Error('Please implement the revokeTicket function.');
+  throw new Error("Please implement the revokeTicket function.");
 }
 
 /**
@@ -31,7 +39,7 @@ export function revokeTicket(visitor) {
  * @returns {string} ticket status
  */
 export function ticketStatus(tickets, ticketId) {
-  throw new Error('Please implement the ticketStatus function.');
+  throw new Error("Please implement the ticketStatus function.");
 }
 
 /**
@@ -43,7 +51,7 @@ export function ticketStatus(tickets, ticketId) {
  * @returns {string} ticket status
  */
 export function simpleTicketStatus(tickets, ticketId) {
-  throw new Error('Please implement the simpleTicketStatus function.');
+  throw new Error("Please implement the simpleTicketStatus function.");
 }
 
 /**
@@ -53,5 +61,5 @@ export function simpleTicketStatus(tickets, ticketId) {
  * @returns {string | undefined} version
  */
 export function gtcVersion(visitor) {
-  throw new Error('Please implement the gtcVersion function.');
+  throw new Error("Please implement the gtcVersion function.");
 }
