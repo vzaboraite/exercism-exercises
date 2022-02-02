78245d8c57f50efb4beb9fa703a4cc13509bdb11
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-02-02 17:12:11 +0000

    Write cookingStatus(), pass the tests.

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -10,3 +10,18 @@
  *   ...
  * }
  */
+
+/**
+ * @param {number} time
+ * @returns {string} cooking status message
+ */
+
+export function cookingStatus(time) {
+  if (time === 0) {
+    return "Lasagna is done.";
+  } else if (!time) {
+    return "You forgot to set the timer.";
+  } else {
+    return "Not done, please wait.";
+  }
+}
