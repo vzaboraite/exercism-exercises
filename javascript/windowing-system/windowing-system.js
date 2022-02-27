9819c69635acdf7cb0b9cd8e03c62d36d3d9931f
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-02-27 00:00:01 +0000

    Setup windowing system exercise

---
+++ javascript/windowing-system/windowing-system.js
@@ -0,0 +1,5 @@
+/**
+ * Implement the classes etc. that are needed to solve the
+ * exercise in this file. Do not forget to export the entities
+ * you defined so they are available for the tests.
+ */
