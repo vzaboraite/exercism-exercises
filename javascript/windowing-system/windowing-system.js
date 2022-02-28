b4c0d950ca3f74dd3e2590085eefcf8f15b5062b
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-02-28 22:59:16 +0000

    Define Size for storing the dimensions of the window.

--- javascript/windowing-system/windowing-system.js
+++ javascript/windowing-system/windowing-system.js
@@ -3,3 +3,16 @@
  * exercise in this file. Do not forget to export the entities
  * you defined so they are available for the tests.
  */
+class Size {
+  constructor(width = 80, height = 60) {
+    this.width = width;
+    this.height = height;
+  }
+
+  resize(newWidth, newHeight) {
+    this.width = newWidth;
+    this.height = newHeight;
+  }
+}
+
+export { Size };
