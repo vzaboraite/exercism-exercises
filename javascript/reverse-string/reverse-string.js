2d3a3412ec43f23b2470fd05e7357a75fb7d3923
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-03-09 22:55:01 +0000

    Reverse given string using built-in methods.

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -2,7 +2,18 @@
 // This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+// @param {String} str
+// @returns {String} reversed string
+//
 
-export const reverseString = () => {
-  throw new Error("Remove this statement and implement this function");
+export const reverseString = (str) => {
+  if (str === "") {
+    return "";
+  }
+
+  const splittedStr = str.split("");
+  const reversedArr = splittedStr.reverse();
+  const reversedStr = reversedArr.join("");
+
+  return reversedStr;
 };
