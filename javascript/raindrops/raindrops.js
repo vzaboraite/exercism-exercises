4d62221880388f8ed15761c388197dc243d6a623
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-03-10 23:52:30 +0000

    Convert number into a droplet sound string.

--- javascript/raindrops/raindrops.js
+++ javascript/raindrops/raindrops.js
@@ -2,7 +2,29 @@
 // This is only a SKELETON file for the 'Raindrops' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+// @param {number} number
+// @returns {string|number} a string depending on factor, or same number in string if doesn't have any factor
+//
+// factors and strings:
+// 3: 'Pling'
+// 5: 'Plang'
+// 7: 'Plong'
+//
 
-export const convert = () => {
-  throw new Error("Remove this statement and implement this function");
+const FACTORS = {
+  3: "Pling",
+  5: "Plang",
+  7: "Plong",
+};
+
+export const convert = (number) => {
+  let result = "";
+
+  for (const factor in FACTORS) {
+    if (number % factor === 0) {
+      result += FACTORS[factor];
+    }
+  }
+
+  return result || `${number}`;
 };
