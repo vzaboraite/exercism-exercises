eac56f3b4a5ac69791d0bd8d12b3a0bdd55dd044
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-03-11 21:04:56 +0000

    Convert a phrase to its acronym.

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -2,7 +2,13 @@
 // This is only a SKELETON file for the 'Acronym' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+// @param {string} phrase
+// @returns {string} acronym of phrase
 
-export const parse = () => {
-  throw new Error("Remove this statement and implement this function");
+export const parse = (phrase) => {
+  const words = phrase.split(/[ _\-,]+/);
+  const firstChars = words.map((word) => word.charAt(0).toUpperCase());
+  const acronym = firstChars.join("");
+
+  return acronym;
 };
