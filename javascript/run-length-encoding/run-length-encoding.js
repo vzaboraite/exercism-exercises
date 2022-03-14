df82eecd2676e4cc9036a1709215087a47e2172c
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-03-14 23:03:14 +0000

    Encode original string.

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -2,9 +2,25 @@
 // This is only a SKELETON file for the 'Run Length Encoding' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+// @param {string} str
+// @returns {string} encoded/decoded string
+//
 
-export const encode = () => {
-  throw new Error("Remove this statement and implement this function");
+export const encode = (str) => {
+  let encodedStr = "";
+
+  for (let i = 1, count = 1; i <= str.length; i++) {
+    const currChar = str[i];
+    const prevChar = str[i - 1];
+    if (currChar === prevChar) {
+      count++;
+    } else {
+      encodedStr += (count === 1 ? "" : count) + prevChar;
+      count = 1;
+    }
+  }
+
+  return encodedStr;
 };
 
 export const decode = () => {
